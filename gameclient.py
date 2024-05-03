169fee7277460e75db7f6e44e1ba9d88d7d422f9
jsnodees <202280134@psu.palawan.edu.ph>
2024-05-03 12:59:59 +0800

    debugged and fix the errors of the client

--- gameclient.py
+++ gameclient.py
@@ -4,43 +4,32 @@ import socket
 host = "localhost"
 port = 7777
 
-def play_game():
+while True:
         s = socket.socket()
         s.connect((host, port))
         
-        while True:
-            difficulty_level = input("Please choose a difficulty level: \n "
-                                    "a. Easy (1 - 50) \n "
-                                    "b. Medium (1 - 100) \n "
-                                    "c. Hard (1 - 500) \n"
-                                    "Please enter your choice (a/b/c): ").strip().lower()
-            if difficulty_level in ['a','b','c']:
-                s.sendall(difficulty_level.encode())
-                break
-            else:
-                print("Invallid Choice! Please choose a valid difficulty level (a/b/c): ")
+        data = s.recv(1024)
+        print(data.decode().strip())
+
+        difficulty = input("").strip().lower()
+        s.sendall(difficulty.encode())
+
+        data = s.recv(1024)
+        print(data.decode().strip())
 
         while True:
-            data = s.recv(1024)
-            if not data:
-                break
-            reply = data.decode().strip()
-
-            while True:
                 #let get our input from the user
-                user_input = input(reply).strip()
+                user_input = input("").strip()
                 s.sendall(user_input.encode())
 
                 reply = s.recv(1024).decode().strip()
-                print(reply)
-
 
                 if "Correct" in reply:
-                    s.close()
-                    return
-while True:
-        play_game()
-        play_again = input("Do you want to play again? (Y/N): ").upper()
-        if play_again != "Y":
+                    print(reply)
+                    break
+                print(reply)
+                continue
+        again = input("Do you want to play again? (Y/N): ").lower()
+        if again != "y":
             break
 
